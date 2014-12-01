ede0675e09650f6a3270604d71131b96a3a42f74
koudaiii <cs006061@gmail.com>
2014-12-01 20:11:49 +0900

    fix ntpd

--- site-cookbooks/my_ntp/recipes/default.rb
+++ site-cookbooks/my_ntp/recipes/default.rb
@@ -6,3 +6,20 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+package "ntp" do
+  action :install
+end
+service "ntp_service" do
+  service_name node[:ntp][:service]
+  action [ :enable, :start ]
+end
+
+template "/etc/ntp.conf" do
+  path "/etc/ntp.conf"
+  source "ntp.conf.erb"
+  variables( :ntp_server => "time.nist.gov" )
+  owner "root"
+  group "root"
+  mode 0644
+  notifies :reload, "service[ntp_service]"
+end
