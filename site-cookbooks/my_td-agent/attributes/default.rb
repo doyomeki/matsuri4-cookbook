d4d8106dd05be1509901528f3a4dce3cb9cadec8
koudaiii <cs006061@gmail.com>
2014-12-08 11:57:28 +0900

    add td-gent cookbook

---
+++ site-cookbooks/my_td-agent/attributes/default.rb
@@ -0,0 +1,5 @@
+defalut[:td_agent][:api_key] =  'foo_bar_buz'
+defalut[:td_agent][:plugins] = ["datacounter", "mackerel"]
+defalut[:td_agent][:includes] = true 
+defalut[:td_agent][:default_config] = true
+
